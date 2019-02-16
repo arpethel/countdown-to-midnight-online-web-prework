7f4c9e755bce2089f9c8feb7119f62b6e2cf5fde
Ryan Pethel <arpethel@gmail.com>
2019-02-16 15:10:55 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
-def countdown (int)
-  ticker = int
+def countdown (number)
+  ticker = number
   while ticker > 0
-    puts "#{int} SECOND(S)!"
+    puts "#{number} SECOND(S)!"
     ticker -= 1
   end
   return "HAPPY NEW YEAR!"
